f530384b14a09218546f387ffc71dfccadfe5a4e
Robby <ta7382@gmail.com>
2018-11-24 21:04:16 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,105 @@
+extern crate actix_web;
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+#[allow(unused_imports)]
+use actix_web::middleware::Logger;
+#[allow(unused_imports)]
+use actix_web::{
+    http, http::Method, server, App, HttpRequest, HttpResponse, Json, Path, Responder, Result,
+};
+
+#[macro_use]
+extern crate serde_derive;
+
+extern crate serde;
+extern crate serde_json;
+
+#[derive(Serialize)]
+
+pub struct Todo {
+    id: u32,
+    item: String,
+    done: bool,
+}
+
+pub fn render(_req: HttpRequest) -> Result<Json<Vec<Todo>>> {
+    Ok(Json(vec![
+        Todo {
+            id: 0,
+            item: "Study".to_owned(),
+            done: true,
+        },
+        Todo {
+            id: 1,
+            item: "Work".to_owned(),
+            done: false,
+        },
+    ]))
+}
+
+fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info,info");
+    env_logger::init();
+
+    let url = "localhost:3000";
+    // 展示不同狀態的 log
+    info!("Running server on {}", url);
+    error!("Running server on {}", url);
+    warn!("Running server on {}", url);
+    // debug!("Running server on {}", url);
+    // trace!("Running server on {}", url);
+
+    server::new(|| {
+        App::new()
+            // 啟用中介層日誌
+            // .middleware(Logger::default())
+            // .middleware(Logger::new("%a %{User-Agent}i"))
+            // 使用前綴，後續網址接作用於該底下，同 .prefix("api")
+            .prefix("/api")
+            // 展示前綴
+            .resource("", |r| r.get().f(|_| format!("Prefix With /api")))
+            // 傳送 http 狀態
+            .resource("ok", |r| r.get().f(|_| HttpResponse::Ok()))
+            // 方法使用 Method::GET 判斷，傳送 http 狀態
+            .resource("method", |r| {
+                r.method(Method::GET).f(|_| HttpResponse::Ok())
+            })
+            // 使用 bodu 回傳純文字
+            .resource("body", |r| r.get().f(|_| HttpResponse::Ok().body("body")))
+            // 回傳 JSON Data
+            .resource("/json", |r| r.get().with(render))
+            // 展示不同方法請求、純文字回傳
+            .resource("/text", |r| {
+                r.get().f(|_| format!("GET"));
+                r.post().f(|_| format!("POST"));
+                r.put().f(|_| format!("PUT"));
+                r.delete().f(|_| format!("DELETE"));
+            })
+            // 展示作用域 s1 底下的不同路由與不方法
+            .scope("/s1", |route| {
+                route
+                    .resource("/v1", |r| r.get().f(|_| format!("s1 v1 get")))
+                    .resource("/v2", |r| r.post().f(|_| format!("s1 v2 post")))
+                    .resource("/v3", |r| r.put().f(|_| format!("s1 v3 put")))
+                    .resource("/v4", |r| r.delete().f(|_| format!("s1 v4 delete")))
+            })
+            // 展示作用域 s2 底下同一個路由的不同方法
+            .scope("/s2", |route| {
+                route
+                    .resource("/v1", |r| {
+                        r.get().f(|_| format!("s2 v1 get"));
+                        r.post().f(|_| format!("s2 v1 post"));
+                    })
+                    .resource("/v2", |r| {
+                        r.put().f(|_| format!("s2 v2 put"));
+                        r.delete().f(|_| format!("s2 v2 delete"));
+                    })
+            })
+    })
+    .bind(url)
+    .unwrap()
+    .run();
+}
